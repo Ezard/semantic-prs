3406ee8b37f08753ca0351df8f35938a8ec5a7d9
Ben Ezard <ezard.ben@gmail.com>
2022-06-02 13:00:38 +0100

    feat: trigger check when a PR is reopened (#43)

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -4,5 +4,6 @@ import { handlePullRequestChange } from './handle-pull-request-change';
 export = (app: Probot) => {
   app.on('pull_request.opened', handlePullRequestChange);
   app.on('pull_request.edited', handlePullRequestChange);
+  app.on('pull_request.reopened', handlePullRequestChange);
   app.on('pull_request.synchronize', handlePullRequestChange);
 };
