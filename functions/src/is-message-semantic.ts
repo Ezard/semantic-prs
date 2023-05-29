5d4011520a203e9d2fc4a0327397af8842e2791b
Ben Ezard <ezard.ben@gmail.com>
2023-05-29 18:37:46 +0100

    chore: remove unused trimming of scope (#311)

--- functions/src/is-message-semantic.ts
+++ functions/src/is-message-semantic.ts
@@ -33,13 +33,7 @@ export function isMessageSemantic({
     }
 
     const { scope, type } = commit;
-    const isScopeValid =
-      !scopes ||
-      !scope ||
-      scope
-        .split(',')
-        .map(scope => scope.trim())
-        .every(scope => scopes.includes(scope));
+    const isScopeValid = !scopes || !scope || scope.split(',').every(scope => scopes.includes(scope));
     const isTypeValid = (types.length > 0 ? types : commitTypes).includes(type);
     return isTypeValid && isScopeValid;
   };
