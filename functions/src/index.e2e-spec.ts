d0cb95177ea7003355bc2636b34622e7270c2400
Ben Ezard <ezard.ben@gmail.com>
2023-06-25 11:57:06 +0100

    fix: update function definition to lazily evaluate env vars to resolve deployment errors

---
+++ functions/src/index.e2e-spec.ts
@@ -0,0 +1,26 @@
+describe('semanticPrs', () => {
+  it('should work', () => {
+    const requestListener = jest.fn();
+    const createNodeMiddleware = jest.fn().mockReturnValue(requestListener);
+    const probot = { version: '1.2.3' };
+    jest.mock('probot', () => ({
+      createNodeMiddleware,
+      createProbot: jest.fn().mockReturnValue(probot),
+    }));
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { semanticPrs } = require('./index');
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { app } = require('./app');
+
+    const req = {};
+    const res = { writeHead: jest.fn(), end: jest.fn() };
+
+    semanticPrs(req, res);
+
+    expect(createNodeMiddleware).toHaveBeenCalledTimes(1);
+    expect(createNodeMiddleware).toHaveBeenCalledWith(app, { probot });
+    expect(requestListener).toHaveBeenCalledTimes(1);
+    expect(requestListener).toHaveBeenCalledWith(req, res);
+  });
+});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,6 @@ import { region } from 'firebase-functions';
 import { createNodeMiddleware, createProbot } from 'probot';
 import { app } from './app';
 
-export const semanticPrs = region('europe-west2').https.onRequest(
-  createNodeMiddleware(app, { probot: createProbot() }),
-);
+export const semanticPrs = region('europe-west2').https.onRequest((req, res) => {
+  createNodeMiddleware(app, { probot: createProbot() })(req, res);
+});
