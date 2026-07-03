4316d7c30de23c1d7ea4f216f6605ca1a2fefad5
Ben Ezard <ezard.ben@gmail.com>
2026-07-03 10:49:58 +0100

    Update ESLint config

--- packages/github/bot/eslint.config.mjs
+++ packages/github/bot/eslint.config.mjs
@@ -1,61 +1,11 @@
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import _import from 'eslint-plugin-import';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+// @ts-check
+
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig } from 'eslint/config';
+import prettierEsLint from 'eslint-config-prettier/flat';
+import tsEsLint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+export default defineConfig({
+  files: ['**/*.{js,ts}'],
+  extends: [js.configs.recommended, tsEsLint.configs.recommended, prettierEsLint],
 });
-
-export default [
-  {
-    ignores: ['lib/**/*'],
-  },
-  ...fixupConfigRules(
-    compat.extends(
-      'eslint:recommended',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-      'plugin:@typescript-eslint/recommended',
-    ),
-  ),
-  {
-    plugins: {
-      '@typescript-eslint': fixupPluginRules(typescriptEslint),
-      import: fixupPluginRules(_import),
-    },
-
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-
-      parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: 'module',
-
-      parserOptions: {
-        project: 'tsconfig.json',
-      },
-    },
-
-    rules: {
-      'import/no-unresolved': [
-        'error',
-        {
-          ignore: ['^firebase-functions/.+'],
-        },
-      ],
-    },
-  },
-];
